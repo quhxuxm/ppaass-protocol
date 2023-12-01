e845f79f15ae2584253f65b364064a677444113e
quhxuxm <quhxuxm.quh@gmail.com>
2023-12-01 09:38:44 +0800

    refact

--- src/values/tunnel.rs
+++ src/values/tunnel.rs
@@ -1,15 +1,18 @@
-use derive_more::Constructor;
+use derive_more::{Constructor, Display};
 use serde_derive::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug, Constructor)]
+#[derive(Serialize, Deserialize, Debug, Constructor, Eq, PartialEq, Hash, Display)]
+#[display(fmt = "{}://[{}]<=>[{:?}]", tunnel_type, agent_edge_id, proxy_edge_id)]
 pub struct Tunnel {
     agent_edge_id: String,
     proxy_edge_id: Option<String>,
     tunnel_type: TunnelType,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Hash, Display)]
 pub enum TunnelType {
+    #[display(fmt = "tcp")]
     Tcp,
+    #[display(fmt = "udp")]
     Udp,
 }
