9f1a04f2da556f5da548496e71caf759b73ba612
quhxuxm <quhxuxm.quh@gmail.com>
2023-12-01 09:49:19 +0800

    refact

--- src/values/tunnel.rs
+++ src/values/tunnel.rs
@@ -4,9 +4,9 @@ use serde_derive::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug, Constructor, Eq, PartialEq, Hash, Display)]
 #[display(fmt = "{}://[{}]<=>[{:?}]", tunnel_type, agent_edge_id, proxy_edge_id)]
 pub struct Tunnel {
-    agent_edge_id: String,
-    proxy_edge_id: Option<String>,
-    tunnel_type: TunnelType,
+    pub agent_edge_id: String,
+    pub proxy_edge_id: Option<String>,
+    pub tunnel_type: TunnelType,
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Hash, Display)]
