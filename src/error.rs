5f7588870e8cd14871cae812111b645dfd6a2c47
quhxuxm <quhxuxm.quh@gmail.com>
2023-12-24 13:01:02 +0800

    refact

--- src/error.rs
+++ src/error.rs
@@ -4,6 +4,6 @@ use thiserror::Error;
 pub enum ProtocolError {
     #[error("Protocol error happen because of standard io: {_0:?}")]
     StdIo(#[from] std::io::Error),
-    #[error("Protocol error happen because of reason: {_0:?}")]
+    #[error("Protocol error happen because of reason: {_0}")]
     Other(String),
 }
