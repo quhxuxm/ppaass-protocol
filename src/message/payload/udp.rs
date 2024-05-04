596124aa7d13584939101440934a3baba1c4033c
quhxuxm <quhxuxm.quh@gmail.com>
2024-05-04 22:00:24 +0800

    refact

--- src/message/payload/udp.rs
+++ src/message/payload/udp.rs
@@ -6,12 +6,16 @@ use derive_more::Constructor;
 use serde_derive::{Deserialize, Serialize};
 
 make_as_bytes! {
-    #[derive(Serialize, Deserialize, Debug, Constructor)]
-    struct AgentUdpPayload {
-        src_address: PpaassUnifiedAddress,
-        dst_address: PpaassUnifiedAddress,
-        data: Bytes,
-        need_response: bool,
+    #[derive(Serialize, Deserialize, Debug)]
+    enum AgentUdpPayload {
+        Init {
+            bind_port: u16
+        },
+        Data {
+            src_address: PpaassUnifiedAddress,
+            dst_address: PpaassUnifiedAddress,
+            data: Bytes,
+        }
     }
 }
 
